e177e746bd0f1a9f44b2ba8ae011359e7204f2e8
yibohao <sxyhao@hotmail.com>
2024-04-14 23:49:37 +0800

    不使用库分页

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -3,9 +3,14 @@ class Api::V1::ItemsController < ApplicationController
         page_number = params[:page] || 1
         per_page = params[:per_page] || 10
     
-        items = Item.page(page_number).per(per_page)
+        items = Item.limit(per_page).offset((page_number.to_i - 1) * per_page.to_i)
         render json: {
-            resources: items
+            resources: items,
+            pager: {
+                page_number: page_number,
+                per_page: per_page,
+                count: Item.count
+            }
         }
     end
 
