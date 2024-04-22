c09593bb77c0765e299b0741adcb268e583b742f
yibohao <sxyhao@hotmail.com>
2024-04-22 21:24:06 +0800

    测试邮箱

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,7 @@ class UserMailer < ApplicationMailer
     default from: '735668747@qq.com'
 
     def welcome_email
+      @code = "123456"
       mail(to: '735668747@qq.com', subject: 'Hi')
     end
 end
